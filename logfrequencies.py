15582a37ca2414f9e5ed35a2ffeada9814cabf94
Denise Draper <draperd@acm.org>
2022-06-02 21:27:43 -0700

    refactoring

--- logfrequencies.py
+++ logfrequencies.py
@@ -2,31 +2,25 @@ import re
 
 strip_digits = str.maketrans("","","0123456789")
 
-all_pattern_counts = {}
-all_pattern_exemplars = {}
+def count_pattern(s: str, counts, exemplars):
+    """Increment the count for the pattern of s"""
 
-def clear_patterns():
-    global all_pattern_counts, all_pattern_exemplars
-    all_pattern_counts = {}
-    all_pattern_exemplars = {}
-
-def add_pattern(s: str):
-    # Quick and dirty normalization:
-    # To make log strings comparable to one another, delete the digits.  This will take care of dates, times, pids, etc.
+    # Generalize s to pattern ns
+    # Quick and dirty generalization: delete the digits.  This will take care of dates, times, pids, etc.
     # It won't get month names if they are spelled out.  And of course some log messages may have other differing parts like usernames, etc.
     ns = s.translate(strip_digits)
-    if ns in all_pattern_counts:
-        all_pattern_counts[ns] += 1
+    
+    # Count the pattern ns
+    if ns in counts:
+        counts[ns] += 1
     else:
-        all_pattern_counts[ns] = 1
+        counts[ns] = 1
+        # also set the exemplar
         # the exemplar is a string we will show to users.  we still want to remove
         # digits to help with stability across runs, but we want there to be a placeholder 
         # for them so the exemplar looks more like the string it is replacing.
-        all_pattern_exemplars[ns] = re.sub(r'\d+','x', s.strip())
+        exemplars[ns] = re.sub(r'\d+','x', s.strip())
 
-def process_file(fp):
-    for line in fp:
-        add_pattern(line)
 
 if __name__ == "__main__":
 
@@ -35,9 +29,11 @@ if __name__ == "__main__":
     parser.add_argument('logfile', type=argparse.FileType('r'))
     args = parser.parse_args()
 
-    process_file(args.logfile)
+    counts = {}
+    exemplars = {}
+    for line in args.logfile:
+        count_pattern(line, counts, exemplars)
 
-    for k in sorted(all_pattern_exemplars.keys()):
-        count = all_pattern_counts[k]
-        name = all_pattern_exemplars[k]
-        print(f"{count:10}: {name}")
+
+    for k in sorted(counts.keys()):
+        print(f"{counts[k]:10}: {exemplars[k]}")
